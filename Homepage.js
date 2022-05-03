6a2b1e47cbbe6e3c5de5e53493a9b3346e6f7c98
hbhushan3 <77566186+hbhushan3@users.noreply.github.com>
2022-05-02 21:47:57 -0500

    Update Homepage.js

--- Homepage.js
+++ Homepage.js
@@ -1,3 +1,4 @@
+
 function loadXMLDoc(funct) {
     //Code to do XML HTTP request (see slides) goes here
     var xhttp = new XMLHttpRequest();
@@ -258,23 +259,11 @@ function fillEmptySections()
 
 }
 
-//Get the button
-var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    var mybutton = document.getElementById("myBtn");
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
